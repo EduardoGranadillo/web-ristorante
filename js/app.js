01ddfcea4943f8a3aed07ac29572ad582cf1da7e
Eduardo Granadillo <elgc16@gmail.com>
2024-05-13 21:56:55 -0300

    hacemos que las imagenes cargen con javascript

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('.hamburguesa');
 
 const navegacion = document.querySelector('.navegacion');
 
-
+const imagenes = document.querySelectorAll('img');
 
 
 
@@ -36,6 +36,11 @@ const botonCerrar = () =>{
     navegacion.appendChild(btnCerrar);
     cerrarMenu(btnCerrar,overlay);
 }
+
+imagenes.forEach(imagen=>{
+    imagen.src = imagen.dataset.src;
+});
+
 
 const cerrarMenu = (boton, overlay) =>{
     boton.addEventListener('click',()=>{
